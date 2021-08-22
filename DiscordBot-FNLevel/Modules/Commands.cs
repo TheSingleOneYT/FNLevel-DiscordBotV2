99ca8ca171180b6f00b0c86f4718a9f71541a4e8
TheSingleOne <81616417+TheSingleOneYT@users.noreply.github.com>
2021-08-22 15:58:02 +0100

    Add 1.0 Files

---
+++ DiscordBot-FNLevel/Modules/Commands.cs
@@ -0,0 +1,59 @@
+﻿using Discord.Commands;
+using Fortnite_API;
+using Fortnite_API.Objects.V1;
+using System.Threading.Tasks;
+
+namespace DiscordBot_FNLevel.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        //Begin commands
+        [Command("lvl")]
+        public async Task GetLevelAsync([Remainder] string s)
+        {
+            var api = new FortniteApiClient();
+
+            var account = Context.Message.ToString().Remove(0, 5);
+
+            var level = await api.V1.Stats.GetBrV2Async(x =>
+            {
+                x.Name = account;
+                x.ImagePlatform = BrStatsV2V1ImagePlatform.All;
+                x.AccountType = BrStatsV2V1AccountType.Epic;
+            });
+
+            if (level.IsSuccess)
+            {
+                await Context.Channel.SendMessageAsync(Context.Message.Author.Mention + ", account '**" + account + "**' is battle pass level **" + level.Data.BattlePass.Level.ToString() + "**.");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Context.Message.Author.Mention + ", could not find account **" + account + "**. Please check your spelling and make sure this is an **EPIC ACCOUNT**.");
+            }
+        }
+
+        [Command("lvl-byID")]
+        public async Task GetIDLevelAsync([Remainder] string s)
+        {
+            var api = new FortniteApiClient();
+
+            var account = Context.Message.ToString().Remove(0, 10);
+
+            var level = await api.V1.Stats.GetBrV2Async(x =>
+            {
+                x.AccountId = account;
+                x.ImagePlatform = BrStatsV2V1ImagePlatform.All;
+                x.AccountType = BrStatsV2V1AccountType.Epic;
+            });
+
+            if (level.IsSuccess)
+            {
+                await Context.Channel.SendMessageAsync(Context.Message.Author.Mention + ", account '**" + account + "**' is battle pass level **" + level.Data.BattlePass.Level.ToString() + "**. (Epic name = **" + level.Data.Account.Name.ToString() + "**).");
+            }
+            else
+            {
+                await Context.Channel.SendMessageAsync(Context.Message.Author.Mention + ", could not find account **" + account + "**. Please check your spelling and make sure this is an **EPIC ACCOUNT**.");
+            }
+        }
+    }
+}
---
+++ DiscordBot-FNLevel/Services/CommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using Discord;
+using Discord.Addons.Hosting;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DiscordBot_FNLevel.Services
+{
+    public class CommandHandler : InitializedService
+    {
+        private readonly IServiceProvider provider;
+        private readonly DiscordSocketClient client;
+        private readonly CommandService service;
+        private readonly IConfiguration configuration;
+
+        public CommandHandler(IServiceProvider provider, DiscordSocketClient client, CommandService service, IConfiguration configuration)
+        {
+            this.provider = provider;
+            this.service = service;
+            this.client = client;
+            this.configuration = configuration;
+        }
+
+        public override async Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            this.client.MessageReceived += OnMessageRecieved;
+            this.service.CommandExecuted += OnCommandExecuted;
+            await this.service.AddModulesAsync(Assembly.GetEntryAssembly(), this.provider);
+        }
+
+        private async Task OnCommandExecuted(Optional<CommandInfo> commandInfo, ICommandContext commandContext, IResult result)
+        {
+            if (result.IsSuccess)
+                return;
+
+            await commandContext.Channel.SendMessageAsync(result.ErrorReason);
+        }
+
+        private async Task OnMessageRecieved(SocketMessage socketMessage)
+        {
+            if (!(socketMessage is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            int argPos = 0;
+
+            if (!message.HasStringPrefix(this.configuration["Prefix"], ref argPos) && !message.HasMentionPrefix(this.client.CurrentUser, ref argPos)) return;
+
+            var context = new SocketCommandContext(this.client, message);
+            await this.service.ExecuteAsync(context, argPos, this.provider);
+        }
+    }
+}
